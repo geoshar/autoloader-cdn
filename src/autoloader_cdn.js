3b9664f390690ae9f4602f4bc3733c2ab50d0c5b
george <georgewebp@gmail.com>
2018-07-09 09:21:08 +0200

    Library added

---
+++ src/autoloader_cdn.js
@@ -0,0 +1,101 @@
+let autoloader_cdn = function (optionsExternal) {
+
+    let options = Object.assign({
+        env: 'production',
+        pkgDependencies: {},
+        loadDependencies: {},
+        urlDependencies: [],
+        loadjs: {
+            enabled: true,
+            name: 'bundle',
+            options: {
+                async: false
+            }
+        },
+        deps: document.currentScript.getAttribute('deps'),
+        depsExclude: document.currentScript.getAttribute('deps-exclude'),
+        depsBefore: document.currentScript.getAttribute('deps-before'),
+        depsAfter: document.currentScript.getAttribute('deps-after'),
+        urlTemplate: 'https://unpkg.com/[pkgName]@[version][path][fileName][fileMin][fileExt]',
+        urlTemplateParse: function (dependency, urlTemplate) {
+            for (let key in dependency) {
+                if (dependency.hasOwnProperty(key)) {
+                    if (typeof dependency[key] === 'string') {
+                        urlTemplate = urlTemplate.replace('[' + key + ']', dependency[key]);
+                    }
+                }
+            }
+            return urlTemplate;
+        }
+    }, optionsExternal);
+    // load scripts before dependencies
+    if (options.depsBefore) {
+        options.depsBefore = options.depsBefore.split(',');
+        options.depsBefore.forEach(function (depBefore) {
+            options.urlDependencies.push(depBefore);
+        });
+    }
+    // if something is excluded
+    if(options.depsExclude){
+        options.depsExclude = options.depsExclude.split(',');
+        options.depsExclude.forEach(function(depRemove){
+            if(options.loadDependencies.hasOwnProperty(depRemove)) {
+                delete options.loadDependencies[depRemove];
+            }
+        });
+    }
+
+    // get the dependencies
+    if (options.deps && options.loadDependencies) {
+        if (options.pkgDependencies) {
+            for (let key in options.pkgDependencies) {
+                if (options.pkgDependencies.hasOwnProperty(key)) {
+                    options.pkgDependencies[key] = options.pkgDependencies[key].replace('^', '');
+                }
+            }
+        }
+        for (let key in options.loadDependencies) {
+            if (options.loadDependencies.hasOwnProperty(key)) {
+                let pkgDependencyVersion = 'version-not-found';
+                if (options.pkgDependencies.hasOwnProperty(key)) {
+                    pkgDependencyVersion = options.pkgDependencies[key];
+                }
+                options.loadDependencies[key] = Object.assign({
+                    pkgName: key,
+                    version: pkgDependencyVersion,
+                    path: '/dist/',
+                    fileName: key,
+                    fileMin: '.min',
+                    fileExt: '.js',
+                    assets: {
+                        //fileName: '',
+                    }
+                }, options.loadDependencies[key]);
+                if (options.env === 'development' || options.loadDependencies[key].fileMinProd === false) {
+                    options.loadDependencies[key].fileMin = '';
+                }
+                let cdnUrlFine = options.urlTemplateParse(options.loadDependencies[key], options.urlTemplate);
+                //console.log(cdnUrlFine);
+                options.urlDependencies.push(cdnUrlFine);
+            }
+        }
+    }
+    // load urls from script tag directly
+    if (options.depsAfter) {
+        options.depsAfter = options.depsAfter.split(',');
+        options.depsAfter.forEach(function (path) {
+            options.urlDependencies.push(path);
+        });
+    }
+    // finally load the scripts
+    if (options.loadjs.enabled) {
+        let loadjs = require('loadjs');
+        loadjs(options.urlDependencies, options.loadjs.name, options.loadjs.options);
+        options.loadjs = loadjs;
+    }
+    return options;
+};
+if(autoloader_cdn_options){
+   autoloader_cdn = autoloader_cdn(autoloader_cdn_options);
+}
+module.exports = autoloader_cdn;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+let path = require('path');
+let env = process.env.NODE_ENV;
+let UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+    mode: env,
+    devtool: (env === "production") ? 'source-map' : 'inline-source-map',
+    entry: {
+        autoloader_cdn: './src/autoloader_cdn.js',
+        "autoloader_cdn.min": './src/autoloader_cdn.js'
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: '[name].js'
+    },
+    plugins: [new CleanWebpackPlugin('dist')],
+    optimization: {
+        minimize: true,
+        minimizer: [new UglifyJsPlugin({
+            include: /\.min\.js$/
+        })]
+    },
+};
